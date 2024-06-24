2f11a7191bf97557facc17cbd146f12db9017268
Joshua Hoblitt <josh@hoblitt.com>
2024-06-24 12:23:08 -0700

    rm manual token param check -- duplicates type assertion

--- manifests/init.pp
+++ manifests/init.pp
@@ -46,9 +46,6 @@ class rke2 (
   if $installation_mode == 'binary' and (!$binary_path or !$binary_version) {
     fail('The vars $binary_version and $binary_path must be set when using the binary installation mode.')
   }
-  if !$token {
-    fail('The var $token must be set.')
-  }
   if !$node_type {
     fail('The var $node_type must be set to either master or agent.')
   }
