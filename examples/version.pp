43fe40c42b054075d28278a4f5a290c951f196f4
Joshua Hoblitt <josh@hoblitt.com>
2024-06-26 14:21:28 -0700

    rm test boilerplate from acceptance test examples

--- examples/version.pp
+++ examples/version.pp
@@ -1,11 +1,3 @@
-class { 'yum':
-  manage_os_default_repos => true,
-}
-
-resources { 'yumrepo':
-  purge => true,
-}
-
 class { 'rke2':
   release_series => '1.28',
   version        => '1.28.2~rke2r1',
--- spec/support/acceptance/setup.rb
+++ spec/support/acceptance/setup.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 def cleanup_rpm
-  on hosts, 'dnf erase -y rke2\*'
+  on hosts, %[
+    dnf erase -y rke2\*
+    rm -f /etc/yum.repos.d/rancher\*
+  ]
 end
 
 RSpec.configure do |c|
