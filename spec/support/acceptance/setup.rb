7db23fc0a70439fdb39b1c897b1587dacbb4c1c8
Joshua Hoblitt <josh@hoblitt.com>
2024-06-26 14:22:56 -0700

    disable config.yaml show_diff
    To prevent the 'token` secret from leaking into the agent logs.

--- spec/support/acceptance/setup.rb
+++ spec/support/acceptance/setup.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 def cleanup_rpm
-  on hosts, %[
-    dnf erase -y rke2\*
-    rm -f /etc/yum.repos.d/rancher\*
-  ]
+  on hosts, %(
+    dnf erase -y rke2\\*
+    rm -f /etc/yum.repos.d/rancher\\*
+  )
 end
 
 RSpec.configure do |c|
